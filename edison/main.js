78644d4a06b9051de8ff41eb1c2ed8b99bb44ae9
Celso Neto <celsopalmeiraneto@gmail.com>
2015-05-11 11:20:19 -0400

    Code from the D-day!!!

---
+++ edison/main.js
@@ -0,0 +1,88 @@
+var mraa   = require('mraa'); //require mraa
+var socket = require('socket.io-client');
+var LCD    = require ('jsupm_i2clcd');
+
+var serverIP = "http://192.168.2.2:3700";
+var io = socket(serverIP);
+
+var portRelay   = [5];
+var relays      = [];
+var portButtons = [2];
+var buttons = [];
+
+var applianceName = ["TV"];
+
+var processingRequest = false;
+var delay = 100;
+
+
+var light = new mraa.Aio(0);
+var myLCD = new LCD.Jhd1313m1(6, 0x3E, 0x62);
+
+
+for(i=0;i<portRelay.length;i++){
+    relays.push(new mraa.Gpio(portRelay[i]));
+    relays[i].dir(mraa.DIR_OUT);
+}
+
+for(i=0;i<portButtons.length;i++){
+    buttons.push(new mraa.Gpio(portButtons[i]));
+    buttons[i].dir(mraa.DIR_IN);
+}
+
+
+
+var watchButton = setInterval(periodicActivity,delay);
+
+
+function periodicActivity() //
+{
+    for(i=0;i<buttons.length;i++){
+        var res = buttons[i].read();
+        if(res>0 && !processingRequest){
+          processingRequest = true;
+          clearInterval(watchButton);
+          askPermission(i);
+        }
+    }
+}
+
+io.on("on", function(msg) {
+    toogleRelay(msg["button"], true);
+});
+
+io.on("off", function(msg) {
+  toogleRelay(msg["button"], false);
+});
+
+function askPermission(button){
+    console.log("Button pressed: "+button);
+    msgToUser(button);
+    io.emit("request",{status:button});
+    processingRequest = false;
+    watchButton = setInterval(periodicActivity,delay);
+
+}
+
+function toogleRelay(button, onOff){
+  if(onOff){
+    relays[button].write(1);
+  }else{
+    relays[button].write(0);
+  }
+}
+
+function msgToUser(button){
+  lcdMessage = "Turn on the "+applianceName[button];
+  myLCD.setCursor(0,0);
+  myLCD.write(lcdMessage);
+  //myLCD.setCursor(1,0);
+  //myLCD.write(lcdMessage.substring(16,16));
+  setTimeout(function(){
+    myLCD.clear();
+  },5000)
+}
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,32 +1,64 @@
-var mraa = require('mraa'); // require mraa
-console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the Intel XDK console
-
-var myOnboardLed = new mraa.Gpio(13);
-myOnboardLed.dir(mraa.DIR_OUT);
-var buttonEdison = new mraa.Gpio(2);
-buttonEdison.dir(mraa.DIR_IN);
-
 var express = require("express"); // importing the express library
+var nodemailer = require('nodemailer');
 var app = express(); // Initializing the express
 var port = 3700; // setting the port
+
+var devices = {};
 
 app.get("/", function(req, res) {
     res.sendFile("index.html", {root: "."});
 });
+app.use(express.static('.'));
 
 var io = require("socket.io").listen(app.listen(port));
 
 io.on('connection', function(socket) {
-    console.log("User connected!");
-    socket.broadcast.on("on", function(msg) {myOnboardLed.write(1); console.log(msg)});
-    socket.broadcast.on("off", function(msg) {myOnboardLed.write(0); console.log(msg)});
+/*  socket.on("log", function(msg){
+        devices[socket.id] = {device: msg["device"], socket: socke, on: false};
+        console.log("Device " + msg["device"] + " connected!");
+    });
+    */
     
-    var buttonStateOld = buttonEdison.read();
-    setInterval(function(){
-        var buttonStateNew = buttonEdison.read();
-        if (buttonStateOld != buttonStateNew && buttonStateNew == 1) {
-            socket.emit("ask", {ask: "May I use the tv?"});
-        }
-        buttonStateOld = buttonStateNew;
-    }, 10);
+    socket.on("yes", function(msg) {
+        socket.broadcast.emit("on", {button: msg["button"]});
+    });
+    
+    socket.on("no", function(msg) {
+        socket.broadcast.emit("off", {button: msg["button"]});
+    });
+    
+    socket.on("request", function(msg) {
+        // This class is sent when I receive a request
+        emailSender({name: "Samantha", surname: 'Monteiro', email: "samanthakem@gmail.com"}, "May I use the Hair Clipper?", "May their or not?", function(error, info){
+            if(error){
+                console.log(error);
+            }
+        });
+    });
+    
+    /*
+    socket.on('disconnect', function() {
+        socket.emit("deviceDesconnected", device[socket.id]);
+    }); */
 });
+
+function emailSender(to, subject, content, callback) {
+    var user = "powertotheparenths@gmail.com";
+    var pass = "789456power0";
+    var transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: user,
+            pass: pass
+        }
+    });
+    
+    var mailOptions = {
+        from: 'PowerToTheParents <'+user+'>',
+        to: to['name'] + " <" + to['email'] + '>',
+        subject: subject,
+        html: "<a href='http://192.168.0.40:3700'>Authorize</a>"
+    };
+
+    transporter.sendMail(mailOptions, callback);
+}
